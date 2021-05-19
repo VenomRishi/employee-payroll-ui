7118d408b2310521f7733cb8e7dc4a895b8dc79a
VenomRishi <rishikesh.mhatre@instarem.com>
2021-05-19 09:45:24 +0530

    added: code for adding employee payroll into localStorage

---
+++ js/add_employee_payroll.js
@@ -0,0 +1,140 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    const text = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    text.addEventListener('input', function () {
+        if (text.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayroll()).name = text.value;
+            textError.textContent = ""
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+
+
+    const salary = document.querySelector("#salary");
+    const output = document.querySelector(".salary-output");
+    output.textContent = salary.value;
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
+    });
+
+});
+
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+}
+
+const resetForm = () => {
+    setValue("#name", "");
+    unsetSelectedValues("[name=profile]");
+    unsetSelectedValues("[name=gender]");
+    unsetSelectedValues("[name=department]");
+    setValue("#salary", "");
+    setValue("#notes", "");
+    setValue("#day", "1");
+    setValue("#month", "Jan");
+    setValue("#year", "2016");
+    setTextValue("#salary-output", "400000");
+}
+
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach((item) => {
+        item.checked = false;
+    });
+};
+
+const setTextValue = (id, value) => {
+    //not used anywhere!
+    const element = document.querySelector(id);
+    element.textContent = value;
+};
+
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+};
+
+
+const createEmployeePayroll = () => {
+    let employeePayroll = new EmployeePayroll();
+    try {
+        employeePayroll.name = getInputValueById("#name");
+    } catch (e) {
+        setTextValue(".text-error", e);
+        throw e;
+    }
+    employeePayroll.profilePic = getSelectedValues("[name=profile]").pop();
+    employeePayroll.gender = getSelectedValues("[name=gender]").pop();
+    employeePayroll.department = getSelectedValues("[name=department]").pop();
+    employeePayroll.salary = getInputValueById("#salary");
+    employeePayroll.note = getInputValueById("#notes");
+    let date =
+        getInputValueById("#day") +
+        "-" +
+        getInputValueById("#month") +
+        "-" +
+        getInputValueById("#year");
+    employeePayroll.startDate = new Date(date);
+    alert(employeePayroll.toString());
+    return employeePayroll;
+}
+
+function createAndUpdateStorage(employeePayrollData) {
+    let employeePayrollList = JSON.parse(
+        localStorage.getItem("employeePayrollList")
+    );
+
+    if (employeePayrollList != undefined) {
+        employeePayrollList.push(employeePayrollData);
+    } else {
+        employeePayrollList = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem(
+        "employeePayrollList",
+        JSON.stringify(employeePayrollList)
+    );
+}
+
+/**
+ * gets the values of all the selected elements
+ */
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach((item) => {
+        if (item.checked) selItems.push(item.value);
+    });
+    return selItems;
+};
+
+/**
+ * querySelector is the newer feature.
+ * It is used when selecting by element name,nesting or class-name.
+ * It will let you find elements with rules that can't be expressedwith getElementById.
+ */
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+};
+
+/**
+* getElementById is better supported than qurySelector method in older versions of the browsers.
+* It will allow to select element by only it's id.
+*/
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+};
